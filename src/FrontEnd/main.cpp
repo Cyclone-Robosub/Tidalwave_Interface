acddbf25236c47da9fa0415b4c25b5b3b406ab3f
jdpieck <jdpieck@ucdavis.edu>
2026-01-31 15:14:01 -0800

    code compiles

--- src/FrontEnd/main.cpp
+++ src/FrontEnd/main.cpp
@@ -1,19 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[]) {
-  QGuiApplication app(argc, argv);
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
 
-  QQmlApplicationEngine engine;
-  const QUrl url(u"../main.qml"_qs);
-  QObject::connect(
-      &engine, &QQmlApplicationEngine::objectCreated, &app,
-      [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-          QCoreApplication::exit(-1);
-      },
-      Qt::QueuedConnection);
-  engine.load(url);
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("test", "Main");
 
-  return app.exec();
+    return app.exec();
 }
