2155c270202680e7d6b380e600a81130603f3bcc
Mihail Marinov <mihail20marinov04@gmail.com>
2026-02-20 17:42:32 -0800

    Restructured IMU struct to make more sense

--- Backend/Model.hpp
+++ Backend/Model.hpp
@@ -1,5 +1,4 @@
 #include <atomic>
-//#include "custom_interfaces/msg/imu.hpp"
 
 struct IMU;
 
@@ -9,19 +8,15 @@ struct DataModel {
 };
 
 struct IMU{
+	std::atomic<double> p_x, p_y, p_z; // point
 	std::atomic<double> o_x, o_y, o_z, o_w; // orientation
+
+	std::atomic<double> vx, vy, vz; // linear velocity
 	std::atomic<double> w_x, w_y, w_z; // angular velocity
+
 	std::atomic<double> a_x, a_y, a_z; // linear acceleration
-	
+
 	std::atomic<double> m_x, m_y, m_z; // magnetic field
 	
 	std::atomic<double> pressure; // pressure
-
-	/*
-	skipped orientation and angular velocity from nav_msgs/Odometry ahrs_database,
-	because its in sensor_msgs/Imu
-	*/
-
-	std::atomic<double> p_x, p_y, p_z; // point
-	std::atomic<double> vx, vy, vz; // linear velocity
 };
