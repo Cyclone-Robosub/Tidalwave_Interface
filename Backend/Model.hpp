540a21e74ec7ec2bd844ec2633b7ce36b1fff625
Mihail Marinov <mihail20marinov04@gmail.com>
2026-02-20 17:41:18 -0800

    Started making the IMU struct

--- Backend/Model.hpp
+++ Backend/Model.hpp
@@ -1,4 +1,27 @@
 #include <atomic>
+//#include "custom_interfaces/msg/imu.hpp"
+
+struct IMU;
+
 struct DataModel {
-  std::atomic<bool> current_mode;
+	std::atomic<bool> current_mode;
+	IMU imu_data;
+};
+
+struct IMU{
+	std::atomic<double> o_x, o_y, o_z, o_w; // orientation
+	std::atomic<double> w_x, w_y, w_z; // angular velocity
+	std::atomic<double> a_x, a_y, a_z; // linear acceleration
+	
+	std::atomic<double> m_x, m_y, m_z; // magnetic field
+	
+	std::atomic<double> pressure; // pressure
+
+	/*
+	skipped orientation and angular velocity from nav_msgs/Odometry ahrs_database,
+	because its in sensor_msgs/Imu
+	*/
+
+	std::atomic<double> p_x, p_y, p_z; // point
+	std::atomic<double> vx, vy, vz; // linear velocity
 };
